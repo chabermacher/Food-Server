00df04448ada5422282e3a2699a9c0157e314b2a
Cecil Habermacher <29548074+chabermacher@users.noreply.github.com>
2018-04-12 13:54:34 -0500

    Restoring commented-out content for testing.

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -9,27 +9,27 @@ module.exports = {
     // put the bit that calls the yelp API here. 
   }
 
-  // findAll: function(req, res) {
-  //   const params = Object.assign(
-  //     { api_key: "9b3adf57854f4a19b7b5782cdd6e427a" },
-  //     req.query
-  //   );
-  //   axios
-  //     .get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
-  //       params
-  //     })
-  //     .then(response => {
-  //       db.Article
-  //         .find()
-  //         .then(dbArticles =>
-  //           response.data.response.docs.filter(article =>
-  //             dbArticles.every(
-  //               dbArticle => dbArticle._id.toString() !== article._id
-  //             )
-  //           )
-  //         )
-  //         .then(articles => res.json(articles))
-  //         .catch(err => res.status(422).json(err));
-  //     });
-  // }
+  findAll: function(req, res) {
+    const params = Object.assign(
+      { api_key: "9b3adf57854f4a19b7b5782cdd6e427a" },
+      req.query
+    );
+    axios
+      .get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
+        params
+      })
+      .then(response => {
+        db.Article
+          .find()
+          .then(dbArticles =>
+            response.data.response.docs.filter(article =>
+              dbArticles.every(
+                dbArticle => dbArticle._id.toString() !== article._id
+              )
+            )
+          )
+          .then(articles => res.json(articles))
+          .catch(err => res.status(422).json(err));
+      });
+  }
 };
